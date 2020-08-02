82e260455ff589e8c642bfe9c8644b320f0e0e9d
Kyle Melton <kalm42@gmail.com>
2020-08-02 10:29:36 -0600

    add privacy policy

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,303 @@
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import './terms.scss'
+
+const privacyPolicy = () => {
+  return (
+    <Layout>
+      <SEO title="Privacy Policy" keywords={[`privacy policy`, `web design`]} />
+      <div className="terms">
+        <h1>Privacy Policy</h1>
+        <p>
+          <small>Last updated August 02, 2020</small>
+        </p>
+        <p>
+          Thank you for choosing to be part of our community at Barbara Melton. We are committed to protecting your
+          personal information and your right to privacy. If you have any questions or concerns about our policy, or our
+          practices with regards to your personal information, please contact us at barbara.melton@gmail.com.
+        </p>
+        <p>
+          When you visit our website https://barbaramelton.com, and use our services, you trust us with your personal
+          information. We take your privacy very seriously. In this privacy notice, we describe our privacy policy. We
+          seek to explain to you in the clearest way possible what information we collect, how we use it and what rights
+          you have in relation to it. We hope you take some time to read through it carefully, as it is important. If
+          there are any terms in this privacy policy that you do not agree with, please discontinue use of our Sites and
+          our services.
+        </p>
+        <p>
+          This privacy policy applies to all information collected through our website (such as
+          https://barbaramelton.com), and/or any related services, sales, marketing or events (we refer to them
+          collectively in this privacy policy as the "Sites"). Please read this privacy policy carefully as it will help
+          you make informed decisions about sharing your personal information with us.
+        </p>
+        <h2>Table of contents</h2>
+        <nav>
+          <ol>
+            <li>
+              <a href="#infocollect">What Information Do We Collection?</a>
+            </li>
+            <li>
+              <a href="#infouse">How do we use your information?</a>
+            </li>
+            <li>
+              <a href="#infoshare">Will your information be shared with anyone?</a>
+            </li>
+            <li>
+              <a href="#whoshare">Who will your information be shared with?</a>
+            </li>
+            <li>
+              <a href="#cookies">Do we use cookies and other tracking technologies?</a>
+            </li>
+            <li>
+              <a href="#googlemaps">Do we use google maps?</a>
+            </li>
+            <li>
+              <a href="#inforetain">How long do we keep your information?</a>
+            </li>
+            <li>
+              <a href="#infosafe">How do we keep your information safe?</a>
+            </li>
+            <li>
+              <a href="#infominors">Do we collect information from minors?</a>
+            </li>
+            <li>
+              <a href="#privacyrights">What are your privacy right?</a>
+            </li>
+            <li>
+              <a href="#DNT">Controls for do-not-track features</a>
+            </li>
+            <li>
+              <a href="#caresidents">Do California residents have specific privacy rights?</a>
+            </li>
+            <li>
+              <a href="#policyupdates">Do we make updates to this policy?</a>
+            </li>
+            <li>
+              <a href="#contact">How can you contact us about this policy?</a>
+            </li>
+          </ol>
+        </nav>
+        <h3 id="infocollect">1. What Information Do We Collection?</h3>
+        <h4>Information automatically collected</h4>
+        <p>
+          In Short: Some information – such as IP address and/or browser and device characteristics – is collected
+          automatically when you visit our Sites.
+        </p>
+        <p>
+          We automatically collect certain information when you visit, use or navigate the Sites. This information does
+          not reveal your specific identity (like your name or contact information) but may include device and usage
+          information, such as your IP address, browser and device characteristics, operating system, language
+          preferences, referring URLs, device name, country, location, information about how and when you use our Sites
+          and other technical information.
+        </p>
+        <p>
+          This information is primarily needed to maintain the security and operation of our Sites, and for our internal
+          analytics and reporting purposes. Like many businesses, we also collect information through cookies and
+          similar technologies.
+        </p>
+        <h4>Information collected from other sources</h4>
+        <p>
+          In Short: We may collect limited data from public databases, marketing partners, and other outside sources.
+        </p>
+        <p>
+          We may obtain information about you from other sources, such as public databases, joint marketing partners, as
+          well as from other third parties. Examples of the information we receive from other sources include: social
+          media profile information ; marketing leads and search results and links, including paid listings (such as
+          sponsored links).
+        </p>
+        <h3 id="infouse">2. How do we use your information?</h3>
+        <p>
+          In Short: We process your information for purposes based on legitimate business interests, the fulfillment of
+          our contract with you, compliance with our legal obligations, and/or your consent.
+        </p>
+        <p>We use personal information collected via our Sites for a variety of business purposes described below.</p>
+        <p>
+          We process your personal information for these purposes in reliance on our legitimate business interests, in
+          order to enter into or perform a contract with you, with your consent, and/or for compliance with our legal
+          obligations. We indicate the specific processing grounds we rely on next to each purpose listed below.
+        </p>
+        <p>We use the information we collect or receive: For other Business Purposes.</p>
+        <p>
+          We may use your information for other Business Purposes, such as data analysis, identifying usage trends,
+          determining the effectiveness of our promotional campaigns and to evaluate and improve our Sites, products,
+          services, marketing and your experience.
+        </p>
+
+        <h3 id="infoshare">3. Will your information be shared with anyone?</h3>
+        <p>
+          In Short: We only share information with your consent, to comply with laws, to protect your rights, or to
+          fulfill business obligations.
+        </p>
+        <p>We may process or share data based on the following legal basis:</p>
+        <p>
+          Consent: We may process your data if you have given us specific consent to use your personal information in a
+          specific purpose.
+        </p>
+        <p>
+          Legitimate Interests: We may process your data when it is reasonably necessary to achieve our legitimate
+          business interests.
+        </p>
+        <p>
+          Performance of a Contract:Where we have entered into a contract with you, we may process your personal
+          information to fulfill the terms of our contract.
+        </p>
+        <p>
+          Legal Obligations: We may disclose your information where we are legally required to do so in order to comply
+          with applicable law, governmental requests, a judicial proceeding, court order, or legal process, such as in
+          response to a court order or a subpoena (including in response to public authorities to meet national security
+          or law enforcement requirements).
+        </p>
+        <p>
+          Vital Interests: We may disclose your information where we believe it is necessary to investigate, prevent, or
+          take action regarding potential violations of our policies, suspected fraud, situations involving potential
+          threats to the safety of any person and illegal activities, or as evidence in litigation in which we are
+          involved. More specifically, we may need to process your data or share your personal information in the
+          following situations: Vendors, Consultants and Other Third-Party Service Providers.
+        </p>
+        <p>
+          We may share your data with third party vendors, service providers, contractors or agents who perform services
+          for us or on our behalf and require access to such information to do that work. Examples include: payment
+          processing, data analysis, email delivery, hosting services, customer service and marketing efforts.{' '}
+        </p>
+        <p>
+          We may allow selected third parties to use tracking technology on the Sites, which will enable them to collect
+          data about how you interact with the Sites over time. This information may be used to, among other things,
+          analyze and track data, determine the popularity of certain content and better understand online activity.
+          Unless described in this Policy, we do not share, sell, rent or trade any of your information with third
+          parties for their promotional purposes.
+        </p>
+        <p>Business Transfers.</p>
+        <p>
+          We may share or transfer your information in connection with, or during negotiations of, any merger, sale of
+          company assets, financing, or acquisition of all or a portion of our business to another company.
+        </p>
+        <h3 id="whoshare">4. Who will your information be shared with?</h3>
+        <p>In Short: We only share information with the following third parties.</p>
+        <p>We only share and disclose your information with the following third parties.</p>
+        <p>
+          We have categorized each party so that you may be easily understand the purpose of our data collection and
+          processing practices. If we have processed your data based on your consent and you wish to revoke your
+          consent, please contact us.
+        </p>
+        <ul>
+          <li>Content Optimization: Google Site Search</li>
+          <li>Web and Mobile Analytics Google Analytics</li>
+          <li>Website Hosting Netlify</li>
+          <li>Website Performance Monitoring Sentry</li>
+        </ul>
+        <h3>5. Do we use cookies and other tracking technologies?</h3>
+        <p>In Short: We may use cookies and other tracking technologies to collect and store your information.</p>
+        <p>
+          We may use cookies and similar tracking technologies (like web beacons and pixels) to access or store
+          information. Specific information about how we use such technologies and how you can refuse certain cookies is
+          set out in our Cookie Policy .
+        </p>
+        <h3 id="googlemaps">6. Do we use google maps?</h3>
+        <p>In Short: Yes, we use Google Maps for the purpose of providing better service.</p>
+        <p>
+          This website, mobile application, or Facebook application uses Google Maps APIs. You may find the Google Maps
+          APIs Terms of Service
+          <a href="https://developers.google.com/maps/terms" target="_blank">
+            here
+          </a>
+          . To better understand Google’s Privacy Policy, please refer to this
+          <a href="https://policies.google.com/privacy" target="_blank">
+            link
+          </a>
+          . By using our Maps API Implementation, you agree to be bound by Google’s Terms of Service.
+        </p>
+
+        <h3 id="inforetain">7. How long do we keep your information?</h3>
+        <p>
+          In Short: We keep your information for as long as necessary to fulfill the purposes outlined in this privacy
+          policy unless otherwise required by law.
+        </p>
+        <p>
+          We will only keep your personal information for as long as it is necessary for the purposes set out in this
+          privacy policy, unless a longer retention period is required or permitted by law (such as tax, accounting or
+          other legal requirements). No purpose in this policy will require us keeping your personal information for
+          longer than 2 years. When we have no ongoing legitimate business need to process your personal information, we
+          will either delete or anonymize it, or, if this is not possible (for example, because your personal
+          information has been stored in backup archives), then we will securely store your personal information and
+          isolate it from any further processing until deletion is possible.
+        </p>
+        <h3 id="infosafe">8. How do we keep your information safe?</h3>
+        <p>
+          In Short: We aim to protect your personal information through a system of organizational and technical
+          security measures.
+        </p>
+        <p>
+          We have implemented appropriate technical and organizational security measures designed to protect the
+          security of any personal information we process. However, please also remember that we cannot guarantee that
+          the internet itself is 100% secure. Although we will do our best to protect your personal information,
+          transmission of personal information to and from our Sites is at your own risk. You should only access the
+          services within a secure environment.
+        </p>
+        <h3 id="infominors">9. Do we collect information from minors?</h3>
+        <p>In Short: We do not knowingly collect data from or market to children under 18 years of age.</p>
+        <p>
+          We do not knowingly solicit data from or market to children under 18 years of age. By using the Sites, you
+          represent that you are at least 18 or that you are the parent or guardian of such a minor and consent to such
+          minor dependent’s use of the Sites. If we learn that personal information from users less than 18 years of age
+          has been collected, we will deactivate the account and take reasonable measures to promptly delete such data
+          from our records. If you become aware of any data we have collected from children under age 18, please contact
+          us at barbara.melton@gmail.com .
+        </p>
+        <h3 id="privacyrights">10. What are your privacy right?</h3>
+        <p>
+          In Short: You may review, change, or terminate your account at any time. If you are resident in the European
+          Economic Area and you believe we are unlawfully processing your personal information, you also have the right
+          to complain to your local data protection supervisory authority. You can find their contact details here:
+          <a href="http://ec.europa.eu/justice/data-protection/bodies/authorities/index_en.htm" target="_blank">
+            http://ec.europa.eu/justice/data-protection/bodies/authorities/index_en.htm
+          </a>
+        </p>
+        <h3 id="DNT">11. Controls for do-not-track features</h3>
+        <p>
+          Most web browsers and some mobile operating systems and mobile applications include a Do-Not-Track (“DNT”)
+          feature or setting you can activate to signal your privacy preference not to have data about your online
+          browsing activities monitored and collected. No uniform technology standard for recognizing and implementing
+          DNT signals has been finalized. As such, we do not currently respond to DNT browser signals or any other
+          mechanism that automatically communicates your choice not to be tracked online. If a standard for online
+          tracking is adopted that we must follow in the future, we will inform you about that practice in a revised
+          version of this Privacy Policy.
+        </p>
+        <h3 id="caresidents">12. Do California residents have specific privacy rights?</h3>
+        <p>
+          In Short: Yes, if you are a resident of California, you are granted specific rights regarding access to your
+          personal information.
+        </p>
+        <p>
+          California Civil Code Section 1798.83, also known as the “Shine The Light” law, permits our users who are
+          California residents to request and obtain from us, once a year and free of charge, information about
+          categories of personal information (if any) we disclosed to third parties for direct marketing purposes and
+          the names and addresses of all third parties with which we shared personal information in the immediately
+          preceding calendar year. If you are a California resident and would like to make such a request, please submit
+          your request in writing to us using the contact information provided below. If you are under 18 years of age,
+          reside in California, and have a registered account with the Sites, you have the right to request removal of
+          unwanted data that you publicly post on the Sites. To request removal of such data, please contact us using
+          the contact information provided below, and include the email address associated with your account and a
+          statement that you reside in California. We will make sure the data is not publicly displayed on the Sites,
+          but please be aware that the data may not be completely or comprehensively removed from our systems.
+        </p>
+        <h3 id="policyupdates">13. Do we make updates to this policy?</h3>
+        <p>In Short: Yes, we will update this policy as necessary to stay compliant with relevant laws.</p>
+        <p>
+          We may update this privacy policy from time to time. The updated version will be indicated by an updated
+          “Revised” date and the updated version will be effective as soon as it is accessible. If we make material
+          changes to this privacy policy, we may notify you either by prominently posting a notice of such changes or by
+          directly sending you a notification. We encourage you to review this privacy policy frequently to be informed
+          of how we are protecting your information.
+        </p>
+        <h3 id="contact">14. How can you contact us about this policy?</h3>
+        <p>
+          If you have questions or comments about this policy, you may email us at barbara.melton@gmail.com or by post
+          to: Barbara Melton 1004 South Avenue B Denton TX 76201 United States
+        </p>
+      </div>
+    </Layout>
+  )
+}
+
+export default privacyPolicy
